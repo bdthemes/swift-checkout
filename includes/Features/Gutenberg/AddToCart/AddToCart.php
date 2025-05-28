f0d12ffcbf7a5592adcac4bd78c0d02807eb4f89
ABU TALIB <abutalib4590@gmail.com>
2025-05-28 16:12:59 +0600

    code updated

--- includes/Features/Gutenberg/AddToCart/AddToCart.php
+++ includes/Features/Gutenberg/AddToCart/AddToCart.php
@@ -2,4 +2,10 @@
 
 use SwiftCheckout\Renders\AddToCart;
 
-echo wp_kses_post(AddToCart::get_markup('gutenberg', $attributes, $block)); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+$markup = AddToCart::get_markup('gutenberg', $attributes, $block);
+if ($markup !== null) {
+    echo wp_kses_post($markup); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+} else {
+    echo ''; // Return empty string if markup is null
+}
